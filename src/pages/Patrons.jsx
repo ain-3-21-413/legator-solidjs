fa0fdf4c28f30220a4892fdd3a1d6f3d8be7861f
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-05-15 23:20:36 +0600

    feat: add general info window

--- src/pages/Patrons.jsx
+++ src/pages/Patrons.jsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Heading, IconButton, Input, SimpleOption, SimpleSelect, VStack } from "@hope-ui/solid";
+import { Button, HStack, Heading, IconButton, Image, Input, SimpleGrid, SimpleOption, SimpleSelect, TagLabel, Text, VStack } from "@hope-ui/solid";
 import { FaSolidLock } from 'solid-icons/fa';
 import { FaSolidLockOpen } from 'solid-icons/fa';
 import { FaSolidPlus } from 'solid-icons/fa';
@@ -93,7 +93,15 @@ const data = {
             date: "01/01/2023",
             name: "Amir Asanov",
         },
-    ]
+    ],
+    info: {
+        stats: {
+            outs: 2,
+            holds: 1,
+            suggestions: 1,
+            lastUse: "01/01/2023",
+        }
+    }
 }
 
 export default function Patrons() {
@@ -165,8 +173,95 @@ export default function Patrons() {
                         </VStack>
                     </VStack>
                 </VStack>
-                <VStack flex={"1"} h="$full" backgroundColor={"green"}>
-
+                <VStack flex={"1"} h="$full" backgroundColor={"white"} p={"$3"}>
+                    <HStack w={"$full"} justifyContent={"space-between"} gap={"$3"}>
+                        <Image src="https://fakeimg.pl/155x200" h={"200px"} w={"155px"} fit="cover" />
+                        <VStack w={"$full"} alignItems={"start"}>
+                            <Text>
+                                First | Middle | Last Name
+                            </Text>
+                            <HStack w={"$full"} gap={"$1"}>
+                                <Input backgroundColor={"$blackAlpha5"} />
+                                <Input backgroundColor={"$blackAlpha5"} />
+                                <Input backgroundColor={"$blackAlpha5"} />
+                            </HStack>
+                            <HStack w={"$full"} gap={"$1"}>
+                                <VStack alignItems={"start"} flex={"1"}>
+                                    <label for="barcode">
+                                        Barcode
+                                    </label>
+                                    <Input id="barcode" backgroundColor={"$blackAlpha5"} />
+                                </VStack>
+                                <VStack alignItems={"start"} flex={"1"}>
+                                    <label for="site">
+                                        Site
+                                    </label>
+                                    <SimpleSelect id="site" backgroundColor={"$blackAlpha5"} defaultValue={"one"}>
+                                        <SimpleOption value={"one"}>
+                                            One
+                                        </SimpleOption>
+                                    </SimpleSelect>
+                                </VStack>
+                            </HStack>
+                            <HStack w={"$full"} gap={"$1"}>
+                                <VStack alignItems={"start"} flex={"1"}>
+                                    <label for="site">
+                                        Status
+                                    </label>
+                                    <SimpleSelect id="status" backgroundColor={"$blackAlpha5"} defaultValue={"active"}>
+                                        <SimpleOption value={"active"}>
+                                            Active
+                                        </SimpleOption>
+                                    </SimpleSelect>
+                                </VStack>
+                                <VStack alignItems={"start"} flex={"1"}>
+                                    <label for="policy">
+                                        Policy
+                                    </label>
+                                    <SimpleSelect id="policy" backgroundColor={"$blackAlpha5"} defaultValue={"basicStudent"}>
+                                        <SimpleOption value={"basicStudent"}>
+                                            Basic Student
+                                        </SimpleOption>
+                                    </SimpleSelect>
+                                </VStack>
+                            </HStack>
+                        </VStack>
+                        <SimpleGrid columns={2} w={"300px"}>
+                            <VStack p={"$1"} borderRightWidth={"1px"} borderBottomWidth={"1px"}>
+                                <Text textTransform={"uppercase"}>
+                                    Out
+                                </Text>
+                                <Text>
+                                    {data.info.stats.outs} 
+                                </Text>
+                            </VStack>
+                            <VStack p={"$1"} borderBottomWidth={"1px"}>
+                                <Text textTransform={"uppercase"}>
+                                    Hold
+                                </Text>
+                                <Text>
+                                    {data.info.stats.holds} 
+                                </Text>
+                            </VStack>
+                            <VStack p={"$1"} borderRightWidth={"1px"}>
+                                <Text textTransform={"uppercase"}>
+                                    Suggestion
+                                </Text>
+                                <Text>
+                                    {data.info.stats.suggestions} 
+                                </Text>
+                            </VStack>
+                            <VStack p={"$1"}>
+                                <Text textTransform={"uppercase"}>
+                                    Last Use
+                                </Text>
+                                <Text>
+                                    {data.info.stats.lastUse} 
+                                </Text>
+                            </VStack>
+                        </SimpleGrid>
+                    </HStack>
+                    
                 </VStack>
             </HStack>
         </VStack>
