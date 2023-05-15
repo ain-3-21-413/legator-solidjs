b65eb41ff93d72d42e978f9e91f99feedca652ac
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-05-15 20:10:20 +0600

    feat: add header

--- src/ELibApp.jsx
+++ src/ELibApp.jsx
@@ -1,11 +1,18 @@
-import { Stack } from "@hope-ui/solid";
+import { Box, Stack } from "@hope-ui/solid";
 import Navbar from "./features/Navbar";
+import Header from "./features/Header";
 
 export default function ELibApp() {
 
     return (
         <Stack h="$screenH" w="$screenW" bgColor={"$blackAlpha4"} direction={"row"}>
             <Navbar />
+            <Box display={"flex"} flexDirection={"column"} h="$full" flex={"1"}>
+                <Header />
+                <Box flexGrow={"1"}>
+                    
+                </Box>
+            </Box>
         </Stack>
     )
 }
---
+++ src/features/Header.jsx
@@ -0,0 +1,19 @@
+import { Box, HStack, Image, Text } from "@hope-ui/solid";
+
+const data = {
+    name: "Demo"
+}
+
+export default function Header() {
+
+    return (
+        <HStack h={"$12"} w={"$full"} bgColor={"$accent11"} justifyContent={"end"} px={"$4"}>
+            <HStack gap={"$2"}>
+                <Text color={"$whiteAlpha12"} fontWeight={"$medium"}>
+                    Hi, {data.name}
+                </Text>
+                <Image h={"$8"} src="https://img.freepik.com/free-icon/user_318-159711.jpg"></Image>
+            </HStack>
+        </HStack>
+    )
+}
