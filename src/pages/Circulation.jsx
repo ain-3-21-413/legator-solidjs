48078d7d0eaa29641c0ab1fb961532631ed9c159
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-05-22 13:51:28 +0600

    checkout

--- src/pages/Circulation.jsx
+++ src/pages/Circulation.jsx
@@ -1,10 +1,23 @@
-import { Stack } from "@hope-ui/solid";
+import { HStack, Heading, Input, SimpleOption, SimpleSelect, VStack } from "@hope-ui/solid";
 
 export default function Circulation() {
 
     return (
-        <Stack>
-            Circulation
-        </Stack>
+        <VStack flexGrow={"1"} w={"$full"} alignItems={"start"}>
+            <Heading size={"2xl"} p={"$3"}>
+                Circulation
+            </Heading>
+            <HStack w={"$full"} p={"$3"} pt={"0"} gap={"$1"}>
+                <SimpleSelect w={"40%"} placeholder="Select" backgroundColor={"white"}>
+                    <SimpleOption value={"CHECK_OUT"}>
+                        Check out
+                    </SimpleOption>
+                    <SimpleOption value={"CHECK_IN"}>
+                        Check in
+                    </SimpleOption>
+                </SimpleSelect>
+                <Input />
+            </HStack>
+        </VStack>
     )
 }
